86f71fa9e71704c1c2b7f3bcd84a573eca2faaa1
Gabriel Oliveira <146120482+Gabriel-OliveiraDev@users.noreply.github.com>
2024-06-07 15:16:07 -0300

    Update img for Products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -66,6 +66,6 @@ class Admin::ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def admin_product_params
-      params.require(:product).permit(:name, :description, :price, :category_id, :active)
+      params.require(:product).permit(:name, :description, :price, :category_id, :active, images: [])
     end
 end
